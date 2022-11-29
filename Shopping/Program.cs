d751f06e4efd06691121fdaec755f0c0805532e1
Georg Wolf <Georg.Wolf@BSLINZ2-IT.LOCAL>
2022-11-29 15:36:06 +0100

    Initial commit

---
+++ Shopping/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Zwiebel");
+Console.WriteLine("Nudelblätter");
+Console.WriteLine("Tomaten");
+Console.WriteLine("Käse");
+Console.WriteLine("Milch");
+Console.WriteLine("Butter");
+Console.WriteLine("Mehl");
+
+Console.WriteLine("Rotwein");
+Console.WriteLine("Weißwein");
+Console.WriteLine("Mascarpone");
+Console.WriteLine("Eier");
+Console.WriteLine("Kaffee");
+Console.WriteLine("Biskotten");
